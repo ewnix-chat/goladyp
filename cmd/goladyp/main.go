707396018480d3bfab951ce7f0e5c8a9799236d6
Kevin Thompson <phlux@ewnix.net>
2023-08-13 11:27:10 -0500

    Add CORS headers

--- cmd/goladyp/main.go
+++ cmd/goladyp/main.go
@@ -49,6 +49,15 @@ func checkUsernameExists(username string) (bool, error) {
 }
 
 func sendEmailHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+        w.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS")
+        w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+        if r.Method == http.MethodOptions {
+                w.WriteHeader(http.StatusOK)
+                return
+        }
+
 	if r.Method != http.MethodPost {
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 		return
