b6e4c6ea1235bed510e5c1ea608f59e4bc240072
Kevin Thompson <phlux@ewnix.net>
2023-08-13 08:54:27 -0500

    Add TLS, since that's what we need, and move everything to variables.

--- main.go
+++ main.go
@@ -1,12 +1,12 @@
 package main
 
 import (
+	"crypto/tls"
 	"fmt"
 	"log"
 	"net/http"
 	"net/smtp"
-
-	"github.com/jordan-wright/email"
+	"os"
 )
 
 func sendEmailHandler(w http.ResponseWriter, r *http.Request) {
@@ -23,17 +23,74 @@ func sendEmailHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// Create an email
-	e := email.NewEmail()
-	e.From = "your-email@example.com"
-	e.To = []string{"accounts@ewnix.net"}
-	e.Subject = "New Account Request!"
-	e.Text = fmt.Sprintf("Username: %s\nE-mail: %s", username, emailAddr)
+	fromEmail := os.Getenv("FROM_EMAIL")
+	toEmail := os.Getenv("TO_EMAIL")
+	smtpServer := os.Getenv("SMTP_SERVER")
+	smtpUsername := os.Getenv("SMTP_USERNAME")
+	smtpPassword := os.Getenv("SMTP_PASSWORD")
 
-	// Send the email
-	err := e.Send("smtp.your-server.com:587", smtp.PlainAuth("", "smtp-username", "smtp-password", "smtp.your-server.com"))
+	// Set up TLS configuration
+	tlsConfig := &tls.Config{
+		InsecureSkipVerify: true, // Only use this in development/testing
+		ServerName:         smtpServer,
+	}
+
+	client, err := smtp.Dial(smtpServer + ":465")
 	if err != nil {
-		log.Println("Error sending email:", err)
+		log.Println("Error connecting to SMTP server:", err)
+		http.Error(w, "Error sending email", http.StatusInternalServerError)
+		return
+	}
+	defer client.Close()
+
+	if err = client.StartTLS(tlsConfig); err != nil {
+		log.Println("Error starting TLS:", err)
+		http.Error(w, "Error sending email", http.StatusInternalServerError)
+		return
+	}
+
+	auth := smtp.PlainAuth("", smtpUsername, smtpPassword, smtpServer)
+
+	msg := []byte("To: " + toEmail + "\r\n" +
+		"Subject: New Account Request!\r\n" +
+		"\r\n" +
+		fmt.Sprintf("Username: %s\r\nE-mail: %s", username, emailAddr))
+
+	if err := client.Auth(auth); err != nil {
+		log.Println("Error authenticating:", err)
+		http.Error(w, "Error sending email", http.StatusInternalServerError)
+		return
+	}
+
+	if err := client.Mail(fromEmail); err != nil {
+		log.Println("Error sending mail from:", err)
+		http.Error(w, "Error sending email", http.StatusInternalServerError)
+		return
+	}
+
+	if err := client.Rcpt(toEmail); err != nil {
+		log.Println("Error sending mail to:", err)
+		http.Error(w, "Error sending email", http.StatusInternalServerError)
+		return
+	}
+
+	writer, err := client.Data()
+	if err != nil {
+		log.Println("Error writing data:", err)
+		http.Error(w, "Error sending email", http.StatusInternalServerError)
+		return
+	}
+
+	_, err = writer.Write(msg)
+	if err != nil {
+		log.Println("Error writing message:", err)
+		http.Error(w, "Error sending email", http.StatusInternalServerError)
+		return
+	}
+
+	err = writer.Close()
+	if err != nil {
+		log.Println("Error closing writer:", err)
 		http.Error(w, "Error sending email", http.StatusInternalServerError)
 		return
 	}
